8166bf9898ed00f011ebbd46b12b9733cbd72695
choimj1717 <choiyou1717@gmail.com>
2026-02-23 21:03:00 +0900

    finish_back

--- C++/re_function/test12/test12/test12.cpp
+++ C++/re_function/test12/test12/test12.cpp
@@ -3,21 +3,29 @@
 
 std::string fiasta = "";
 
-int i = 0;
-
 int print_set(int alpha) {
+
+	if (alpha == 0){		
+		std::cout << fiasta << "\"재귀함수가 뭔가요?\"" << std::endl;
+		std::cout << fiasta << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << std::endl;
+		std::cout << fiasta << "라고 답변하였지." << std::endl;
+		return 0;
+	}
+
 	std::cout << fiasta << "\"재귀함수가 뭔가요?\"" << std::endl;
-	std::cout << fiasta << "\"잘 들어보게.옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\"" << std::endl;
+	std::cout << fiasta << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." << std::endl;
 	std::cout << fiasta << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." << std::endl;
 	std::cout << fiasta << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" << std::endl;
 	
-	for (i; i <= alpha; i++) {
-		fiasta += "____";
-	}
-
+	fiasta += "____";
+	
 	print_set(alpha - 1);
+
+	fiasta = fiasta.substr(0, fiasta.length() - 4);
 	
 	std::cout << fiasta << "라고 답변하였지." << std::endl;
+
+	return 0;
 }
 
 int main()
@@ -26,7 +34,7 @@ int main()
 
 	std::cin >> num;
 
-	std::cout << fiasta << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." << std::endl;
+	std::cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." << std::endl;
 
 	print_set(num);
 
