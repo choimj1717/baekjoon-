712d134a50d9973e63b64072858b7147c4561ad9
choi_mj <mjchoi@mobai.co.kr>
2026-02-23 13:56:56 +0900

    Initial commit

---
+++ C++/fibonacch/test1/test1/test1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int fibo(int a) {
+
+	if (a <= 1)
+	{
+		return a;
+	}
+	return fibo(a - 1) + fibo(a - 2);
+}
+        
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	std::cout << fibo(n) << std::endl;
+}
