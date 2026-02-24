876eeaaf0328d372dec248526d9b28050226e418
choi_mj <mjchoi@mobai.co.kr>
2026-02-24 15:42:22 +0900

    Add hanoi project and ignore VS artifacts

---
+++ C++/hanoi/hanoi/hanoi.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int hanoi(int alpha) {
+
+	if (alpha == 0) return 0;
+
+	for (int i = 0; i < alpha; i++) {
+		for(int j  = 0; j < alpha - i; j++) {
+
+		}
+	}
+
+
+	return 0;
+}
+
+int main()
+{
+	int num;
+
+	std::cin >> num;
+
+	hanoi(num);
+
+    return 0;
+}
