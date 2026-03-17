85176d66c8c2b06274fa4bb1876b3c7e21d12503
choi_mj <mjchoi@mobai.co.kr>
2026-03-17 12:45:04 +0900

    las_baeck

--- C++/260316/backjoon_0316/backjoon_0316/backjoon_0316.cpp
+++ C++/260316/backjoon_0316/backjoon_0316/backjoon_0316.cpp
@@ -32,16 +32,112 @@ int main() {
 // 
 // }
 
+//#include <iostream>
+//#include <algorithm>
+//
+//int main(void)
+//{
+//	int num;
+//	int arr[1001] = {};
+//
+//	std::cin >> num;
+//
+//	for (int i = 0; i < num; i++) {
+//		std::cin >> arr[i];
+//	}
+//
+//	std::sort(arr, arr + num);
+//
+//	for (int i = 0; i < num; i++) {
+//		std::cout << arr[i] << std::endl;
+//	}
+//
+//	return 0;
+//
+// }
+
+//#include <iostream>
+//#include <algorithm>
+//
+//int main(void)
+//{
+//	int arr[5] = {};
+//	int avg = 0;
+//
+//	for (int i = 0; i < 5; i++) {
+//		std::cin >> arr[i];
+//	}
+//
+//	std::sort(arr, arr + 5);
+//
+//	for (int i = 0; i < 5; i++) {
+//		avg += arr[i];
+//	}
+//
+//	avg /= 5;
+//
+//	std::cout << avg << std::endl;
+//	std::cout << arr[2] << std::endl;
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <algorithm>
+//
+//int main() {
+//	int first, second;
+//	long arr[10001] = {};
+//
+//	std::cin >> first >> second;
+//
+//	for (int i = 0; i < first; i++) {
+//		std::cin >> arr[i];
+//	}
+//
+//	std::sort(arr, arr + first, std::greater<long>());
+//
+//	std::cout << arr[second - 1] << std::endl;
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    string s;
+//    cin >> s;  
+//
+//    vector<int> arr;
+//
+//    // 숫자 자릿수 분리
+//    for (char c : s) {
+//        arr.push_back(c - '0');
+//    }
+//
+//    // 작은 수부터 정렬
+//    sort(arr.begin(), arr.end(), std::greater<int>());
+//
+//    // 출력
+//    for (int x : arr) {
+//        cout << x;
+//    }
+//
+//    return 0;
+//}
+
 #include <iostream>
+#include <algorithm>
 
-int main(void)
-{
-	int num;
-	std::cin >> num;
+int main() {
+	int x, y, w, h;
+	std::cin >> x >> y >> w >> h;
 
-	for (int i = 0; i < num; i++) {
-
-	}
+	std::cout << std::min({ x, y, w - x, h - y }) << std::endl;
 
 	return 0;
 }
--- python/25206.py
+++ python/25206.py
@@ -1,3 +1,4 @@
-arr = list(map(str, input().split(" ")))
+for i in range(0, 20):
+    arr = list(map(str, input().split(" ")))
 
 print(arr)
